3855455b6b9ca96c0466d2cc1a43dd1b83de5e06
Eric P. Katz <ericpkatz@gmail.com>
2017-05-13 14:55:38 -0400

    separate out files

---
+++ src/components/Product/ProductFilters.js
@@ -0,0 +1,54 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { hashHistory } from 'react-router';
+import ProductFilter from './ProductFilter';
+
+const _ProductFilters = ({ filters, reset })=> {
+  return (
+    <div>
+      <a onClick={ reset }>Reset</a>
+      <div className='row'>
+      {
+        filters.map( (filter, idx) => <ProductFilter key={ idx } filter={ filter } />)
+      }
+      </div>
+    </div>
+  );
+};
+
+const generateFilters = ({ users })=> {
+  const userChoices = users.map( user => ({
+        value: user.id,
+        text: user.name
+      }));
+  userChoices.unshift({ text: 'All', value: -1 });
+
+  const inStockChoices = [
+    { text: 'All', value: -1 },
+    { text: 'In Stock', value: true },
+    { text: 'Out of Stock', value: false },
+  ];
+
+  return [
+    {
+      key: 'userId',
+      choices: userChoices 
+    },
+    {
+      key: 'inStock',
+      choices: inStockChoices 
+    }
+  ];
+};
+
+const mapStateToProps = ( { productSearch, users } )=> {
+  const { currentFilter } = productSearch;
+  return {
+    filters: generateFilters({users}),
+    reset: ()=> {
+      hashHistory.push('/products');
+    },
+  };
+};
+
+export default connect(mapStateToProps)(_ProductFilters);
--- src/components/Product/ProductsPage.js
+++ src/components/Product/ProductsPage.js
@@ -5,41 +5,14 @@ import { connect } from 'react-redux';
 import { setFilter, search } from '../../redux/reducers/productSearchReducer';
 import { hashHistory } from 'react-router';
 
-const Filter = ({ filter, currentFilter, onFilterChange })=> {
-  const onChange = (ev)=> {
-    const value = filter.choices[ev.target.selectedIndex].value;
-    onFilterChange(filter.key, value);
-  };
-  return (
-    <div className='well'>
-      { filter.key }
-      <select onChange={ onChange } className='form-control'>
-        {
-          filter.choices.map( (choice) => <option selected={ currentFilter[filter.key] == choice.value } value={ choice.value } key={ choice.text }>{ choice.text}</option> )
-        }
-      </select>
-    </div>
-  );
-};
+import ProductFilters from './ProductFilters';
 
-const ProductFilter = ({ filters, currentFilter, onFilterChange, reset })=> {
-  return (
-    <div>
-      <a onClick={ reset }>Reset</a>
-      <br />
-      {
-        filters.map( (filter, idx) => <Filter key={ idx } onFilterChange={ onFilterChange } filter={ filter } currentFilter={ currentFilter }/>)
-      }
-    </div>
-  );
-};
 
-const _ProductsPage = ({ searchResults, currentFilter, filters, onFilterChange, reset })=> (
+
+const _ProductsPage = ()=> (
   <div className='well'>
-    { JSON.stringify(currentFilter) }
-    { searchResults.length }
     <ProductForm />
-    <ProductFilter filters={ filters } reset={ reset } currentFilter={ currentFilter } onFilterChange={ onFilterChange }/>
+    <ProductFilters />
     <ProductList />
   </div>
 );
@@ -56,59 +29,12 @@ class ProductsPage extends Component{
   }
   render(){
     return (
-      <_ProductsPage {...this.props } />
+      <_ProductsPage />
     );
   }
 }
 
-const generateFilters = ({ users })=> {
-  const userChoices = users.map( user => ({
-        value: user.id,
-        text: user.name
-      }));
-  userChoices.unshift({ text: 'All', value: -1 });
 
-  const inStockChoices = [
-    { text: 'All', value: -1 },
-    { text: 'In Stock', value: true },
-    { text: 'Out of Stock', value: false },
-  ];
-
-  return [
-    {
-      key: 'userId',
-      choices: userChoices 
-    },
-    {
-      key: 'inStock',
-      choices: inStockChoices 
-    }
-  ];
-};
-
-const mapStateToProps = ( { productSearch, users } )=> {
-  const { searchResults, currentFilter } = productSearch;
-  return {
-    searchResults,
-    currentFilter,
-    filters: generateFilters({users}),
-    reset: ()=> {
-      hashHistory.push('/products');
-    },
-    onFilterChange: (key, value)=> {
-      const change = {};
-      change[key] = value;
-      let newFilter = Object.assign({}, currentFilter, change); 
-      newFilter = Object.keys(newFilter).reduce((memo, key)=> {
-        if(newFilter[key] != -1){
-          memo[key] = newFilter[key];
-        }
-        return memo;
-      }, {});
-      hashHistory.push(`/products/${ encodeURI(JSON.stringify(newFilter))}`);
-    }
-  };
-};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -116,5 +42,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps )(ProductsPage);
-
+export default connect(null, mapDispatchToProps )(ProductsPage);
